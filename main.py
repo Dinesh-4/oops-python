0f4676d92d24c4a8d290d25db4822781e4ee8d70
Dinesh <dinesh46.mv@gmail.com>
2024-01-09 19:55:46 +0530

    without __str__() funtion

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ class Car:
         self.model = model
         self.rego = regno
 
-    
-    def start(self):
-        print("vruuuuu..... {} Engine is Online. Car No: {}".format(self.make, self.rego))
+
+    # def start(self):
+    #     print("vruuuuu..... {} Engine is Online. Car No: {}".format(self.make, self.rego))
 
 
 
+#without __str__() funtion
 
 car1 = Car("BMW", "M4", "TN19 8888")
-car1.rego = "TN19 1234"
-car1.start()
-del car1
-car1.start()
+print(car1)
 
+#output
+#<__main__.Car object at 0x000001BB73BE9E50>
