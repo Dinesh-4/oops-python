e27b40f013b74be8457e30901260a5079ee88db2
Dinesh <dinesh46.mv@gmail.com>
2024-01-09 20:00:29 +0530

    with __str__() funtion

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ class Car:
         self.rego = regno
 
 
+    def __str__(self):
+        return("vruuuuu..... {} Engine is Online. Car No: {}".format(self.make, self.rego))
+
+
     # def start(self):
     #     print("vruuuuu..... {} Engine is Online. Car No: {}".format(self.make, self.rego))
 
 
 
-#without __str__() funtion
+#with __str__() funtion
 
-car1 = Car("BMW", "M4", "TN19 8888")
+car1 = Car("BMW", "M4", 4567 )
 print(car1)
-
-#output
-#<__main__.Car object at 0x000001BB73BE9E50>
