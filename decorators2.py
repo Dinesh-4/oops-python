fb5456290559ad24977919e54647eab6687b7f2f
Dinesh <dinesh46.mv@gmail.com>
2024-01-10 21:03:58 +0530

    final decorator

--- decorators2.py
+++ decorators2.py
@@ -3,16 +3,17 @@ from functools import wraps
 
 def beg(target_function):
     @wraps(target_function)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs ):
         msg = target_function(*args, **kwargs)
         return msg + " Please!!!"
     return wrapper
 
 
 @beg
-def say():
-    msg = "Can you buy me Samosa"
+def say(name, a):
+    msg = f"{name}! Can you buy me {a} Samosa"
     return msg
 
+print(say("Freak", a = 4))
 
-print(say())
+
