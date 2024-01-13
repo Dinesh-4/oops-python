b79b954f04ed87222242aa2232effe8764eed33e
Dinesh <dinesh46.mv@gmail.com>
2024-01-13 16:36:53 +0530

    Oops

--- main.py
+++ main.py
@@ -4,4 +4,43 @@ car1 = Car("BMW", "M4", 4567 )
 car1.start()
 
 car1.batt = 50
+
+# Call with Object Instance
 car1.get_battery()
+
+# Call with Class
+
+Car.get_battery(car1)
+print(Car.type)
+
+a = Car.build_aston("TN33HH1234") #Classmethod
+b = Car.build_db11("TN33RE2345") #Staticmethod
+
+Car.derive_max_speed()
+
+#calling property - getter
+print(a.make)
+
+
+#property setter
+a.make = "Marton"
+a.make = "Volvo"
+print(a.make)
+
+car1.make = "WMB"
+print(car1.make)
+
+#class attribute
+Car.type = "heyden"
+print(Car.type)
+
+# Property delete
+# del a.make
+# print(a.make)
+
+
+#-----------------
+a.type = "Hatchback"
+print("A's Type : " + a.type)
+print(a.make)
+
--- store/Car.py
+++ store/Car.py
@@ -1,14 +1,59 @@
 class Car:
 
+    #Class Attribute
+    type = "Sedan"
+
     def __init__(self ,make, model, regno):
-        self.make = make
+        self._make = make
         self.model = model
         self.rego = regno
         self.batt = 0
+        if self.type == "Sedan":
+            self.breakpow = "100%"
+        elif self.type == "Hatchback":
+            self.breakpow = "75%"
 
     def start(self):
-        print("vruuuuu..... {} Engine is Online. Car No: {}".format(self.make, self.rego))
+        print("vruuuuu..... {} Engine is Online. Car No: {}".format(self._make, self.rego))
 
     def get_battery(self):
         print("Battery : {}%".format(self.batt))
         self.start()
+
+    @classmethod
+    def build_aston(cls, regno):
+        g = cls("Aston Marton", "AB11", regno)
+        g.start()
+        return g
+    
+    @staticmethod
+    def build_db11(regno):
+        cls = Car
+        g = cls("Audi", "A6", regno)
+        g.start()
+        return g
+    
+    @staticmethod
+    def derive_max_speed():
+        print("I am all independent car's @staticmethod")
+
+
+    @property
+    def make(self):
+        return self._make
+    
+    #Property Attribute - this an extentions from above one 
+    @make.getter
+    def make(self):
+        print("Returning {} {}".format(self.model, self.breakpow))
+        return self._make
+    
+    @make.setter
+    def make(self, make):
+        print("Setting make to {}".format(make))
+        self._make = make
+
+    @make.deleter
+    def make(self):
+        print("Deleting {}...".format(self._make))
+        del self._make
